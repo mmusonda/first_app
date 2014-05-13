9d54063017c5fd032f98785f6efb85cd124d1a5a
Mulenga Musonda <mulenga.musonda@northrise.org>
2014-05-13 12:55:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'dbc58cab62f0998dfd7d91b912f268f8a6ab3e93da9259aa25d0e340e151ae3f436927ee281ce20e6369e2bd8fd7791aa862b310b3813490029414aa22f18c1a'
